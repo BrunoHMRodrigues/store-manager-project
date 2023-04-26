8de9079a1bbcc141ae7506e5e94f570c8d72e0ff
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-04-26 16:21:35 -0300

    WIP: Requisite 01 endpoit /products created, next /products/:id

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 // Iniciando
 const express = require('express');
+const productsController = require('./controllers/productsController');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsController.getAll);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
