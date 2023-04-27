4c4199a9fd39a8f32c4e4b4df48fed416fcb6fef
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-04-27 14:45:22 -0300

    WIP: Requisite 05 - developing services tests

--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -1,17 +1,19 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
+const { productsModel } = require('../../../src/models');
 const { productsService } = require('../../../src/services');
-const connection = require('../../../src/models/connection');
-const validateName = require('../../../src/services/validations/validateName');
+// const connection = require('../../../src/models/connection');
+const { validateName } = require('../../../src/services/validations/validateName');
 
 const {
-  mockGetAllProducts,
+  // mockGetAllProducts,
   successAllProducts,
-  mockGetProduct,
+  // mockGetProduct,
   successGetProduct,
-  mockFailProduct,
-  successCreateProduct } = require('../mock/productsMock');
+  // mockFailProduct,
+  // successCreateProduct
+} = require('../mock/productsMock');
 
 afterEach(() => sinon.restore());
 
@@ -19,7 +21,7 @@ describe('Testing Service from Products', function () {
   describe('Cases of success', function () {
     const resultSuccessGetAll = { type: null, message: successAllProducts };
     it('getAll existing data', async function () {
-      sinon.stub(productsService, 'getAll').resolves(resultSuccessGetAll);
+      sinon.stub(productsModel, 'getAll').resolves(resultSuccessGetAll);
 
       const result = await productsService.getAll();
 
@@ -30,7 +32,7 @@ describe('Testing Service from Products', function () {
 
     const resultSuccessGetProduct = { type: null, message: successGetProduct };
     it('getProductById existing id', async function () {
-      sinon.stub(productsService, 'getProductById').resolves(resultSuccessGetProduct);
+      sinon.stub(productsModel, 'getProductById').resolves(resultSuccessGetProduct);
 
       const result = await productsService.getProductById(1);
 
@@ -43,7 +45,7 @@ describe('Testing Service from Products', function () {
     const resultSuccessCreateProduct = { type: null, message: newProduct };
 
     it('createProduct success creating product', async function () {
-      sinon.stub(productsService, 'createProduct').resolves(resultSuccessCreateProduct);
+      sinon.stub(productsModel, 'createProduct').resolves(resultSuccessCreateProduct);
 
       const result = await productsService.createProduct(productData);
 
@@ -56,6 +58,8 @@ describe('Testing Service from Products', function () {
 
   describe('Cases of failure', function () {
     const productData = { "name": "bad" };
+    const newProduct = { "id": 4, "name": "bad" };
+    const resultSuccessCreateProduct = { type: null, message: newProduct };
 
     const resultFailValidateName = {
       type: 'NAME_INVALID',
@@ -65,11 +69,14 @@ describe('Testing Service from Products', function () {
 
     // VERIFY HOW TO DO IT
     it('length of the name of the product lower than 5', async function () {
-      sinon.stub(validateName).resolves(resultFailValidateName);
+      // const validateNameStub = sinon.stub(validateName);
+      // validateNameStub.throws(resultFailValidateName);
+
+      sinon.stub(productsModel, 'createProduct').resolves(resultSuccessCreateProduct);
 
       const result = await productsService.createProduct(productData);
 
-      expect(validateName.calledOnceWith(productData)).to.be.true;
+      // expect(validateNameStub.calledOnceWith(productData)).to.be.true;
       expect(result).to.contains.keys(['type', 'status', 'message']);
       expect(result).to.be.deep.equal(resultFailValidateName);
     });
